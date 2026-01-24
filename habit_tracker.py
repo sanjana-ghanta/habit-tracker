5aed603cb88b8ea35cd365ebcbcaaee5ebcb31db
Sanjana Ghanta <76929510+sanjana-ghanta@users.noreply.github.com>
2026-01-24 18:02:33 -0500

    Update habit_tracker.py
    Changed responses

--- habit_tracker.py
+++ habit_tracker.py
@@ -2,6 +2,6 @@ habit = input("What habit are you tracking? ")
 done = input("Did you complete it today? (y/n) ")
 
 if done == "y":
-    print("Nice job!")
+    print("Yay, good job!")
 else:
-    print("Try again tomorrow.")
+    print("It's okay try again tomorrow!")
